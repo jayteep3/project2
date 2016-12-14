3e03d4edd70085478ccf9e0800120c81314fd72f
JT Palmer <palmerj@sonoma.edu>
2016-12-13 16:28:39 -0800

    close to done with animals add and edit

--- model/animal_dal.js
+++ model/animal_dal.js
@@ -1,7 +1,4 @@
 /**
- * Created by JT on 12/13/2016.
- */
-/**
  * Created by JT on 12/9/2016.
  */
 var mysql   = require('mysql');
@@ -12,16 +9,16 @@ var connection = mysql.createConnection(db.config);
 
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM animals ;';
+    var query = 'SELECT * FROM animals;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.getById = function(animals_id, callback) {
-    var query = 'SELECT * FROM animals WHERE animals_id = ?';
-    var queryData = [animals_id];
+exports.getById = function(animal_id, callback) {
+    var query = 'SELECT * FROM animals WHERE animal_id = ?';
+    var queryData = [animal_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
@@ -29,11 +26,11 @@ exports.getById = function(animals_id, callback) {
 };
 
 exports.insert = function(params, callback) {
-    var query = 'INSERT INTO animals (typeOfAnimal, sex, weight_in_Pounds, breed_name, adopterSSN,homeShelt) VALUES (?, ?, ?, ?, ?, ?)';
+    var query = 'INSERT INTO animals (typeOfAnimal, sex, weight_in_Pounds, breed_name, homeShelt) VALUES (?, ?, ?, ?, ?)';
 
     // the question marks in the sql query above will be replaced by the values of the
     // the data in queryData
-    var queryData = [params.typeOfAnimal, params.sex, params.weight_in_Pounds, params.breed_name, params.adopterSSN, params.homeShelt];
+    var queryData = [params.typeOfAnimal, params.sex, params.weight_in_Pounds, params.breed_name, params.homeShelt];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
@@ -41,9 +38,9 @@ exports.insert = function(params, callback) {
 };
 
 
-exports.delete = function(animals_id, callback) {
-    var query = 'DELETE FROM animals WHERE animals_id = ?';
-    var queryData = [animals_id];
+exports.delete = function(animal_id, callback) {
+    var query = 'DELETE FROM animals WHERE animal_id = ?';
+    var queryData = [animal_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
@@ -52,17 +49,18 @@ exports.delete = function(animals_id, callback) {
 };
 
 exports.update = function(params, callback) {
-    var query = 'UPDATE animals SET typeOfAnimal = ?, sex = ?, weight_in_Pounds = ?, breedName = ?, addopterSSN = ?, homeShelt = ? WHERE animals_id = ?';
-    var queryData = [params.typeOfAnimal, params.sex, params.weight_in_Pounds, params.breedName, params.adopterSSN, params.homeShelt, params.animals_id];
+    var query = 'UPDATE animals SET typeOfAnimal = ?, sex = ?, weight_in_Pounds = ?, breed_name = ?, homeShelt = ? WHERE applicant_id = ?';
+    var queryData = [params.typeOfAnimal, params.sex, params.weight_in_Pounds, params.breed_name, params.homeShelt, params.animal_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.edit = function(animals_id, callback) {
-    var query = 'SELECT * FROM animals WHERE animals_id = ?';
-    var queryData = [animals_id];
+
+exports.edit = function(applicant_id, callback) {
+    var query = 'SELECT * FROM animalss WHERE animal_id = ?';
+    var queryData = [applicant_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
--- routes/animal_routes.js
+++ routes/animal_routes.js
@@ -3,11 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
-var animals_dal = require('../model/animal_dalLab12Example');
-var account_dal = require('../model/breed_dal');
-var skill_dal = require('../model/applicant_dal');
-var company_dal = require('../model/shelter_dal');
-
+var animals_dal = require('../model/animal_dal');
 
 
 // View All animalss
@@ -17,62 +13,44 @@ router.get('/all', function(req, res) {
             res.send(err);
         }
         else {
-            res.render('animal/animalViewAll', { 'result': result });
+            res.render('animal/animalViewAll', { 'result':result });
         }
     });
+
 });
 
 // View the animals for the given id
 router.get('/', function(req, res){
-    if(req.query.animal_id == null) {
-        res.send('animal_id is null');
+    if(req.query.animals_id == null) {
+        res.send('animals_id is null');
     }
     else {
-        animals_dal.getById(req.query.animal_id, function(err,animals){//, animals_skill, animals_company, animals_school) {
+        animals_dal.getById(req.query.animals_id, function(err,result) {
             if (err) {
                 res.send(err);
             }
             else {
-                res.render('animal/animalViewById', {animals: animals}); //, animals_skill: animals_skill, animals_company: animals_company, animals_school: animals_school});
+                res.render('animal/animalViewById', {'result': result});
             }
         });
     }
 });
 
-/*
 // Return the add a new animals form
 router.get('/add', function(req, res){
-    // passing all the query parameters (req.query) to the insert function instead of each individually
-    account_dal.getAll(function(err,account) {
-        company_dal.getAll(function(err, company) {
-            skill_dal.getAll(function(err, skill) {
-                    if (err) {
-                        res.send(err);
-                    }
-                    else {
-                        res.render('animals/animalsAdd', {account: account, company: company,
-                            skill: skill});
-                    }
-                });
-            });
-        });
-    });
-*/
+    res.render('animal/animalAdd');
+});
 
 // insert a animals record
 router.get('/insert', function(req, res){
     // simple validation
-    if(req.query.animal_name == null) {
-        res.send('animals Name must be provided.');
-    }
-    else if(req.query.account_id == null) {
-        res.send('An Account Id must be selected');
+    if(req.query.animalsName == null) {
+        res.send('an animals must be provided.');
     }
     else {
         // passing all the query parameters (req.query) to the insert function instead of each individually
         animals_dal.insert(req.query, function(err,result) {
             if (err) {
-                console.log(err);
                 res.send(err);
             }
             else {
@@ -83,7 +61,28 @@ router.get('/insert', function(req, res){
     }
 });
 
-// Delete a animals for the given animals_id
+router.get('/edit', function(req, res){
+    if(req.query.animals_id == null) {
+        res.send('A animals id is required');
+    }
+    else {
+        animals_dal.edit(req.query.animals_id, function(err, result){
+            console.log(result);
+            //need to change this line
+            res.render('animal/animalUpdate', {animalss: result[0]});//, address: result[1]});
+        });
+    }
+
+});
+
+router.get('/update', function(req, res) {
+    animals_dal.update(req.query, function(err, result){
+        res.redirect(302, '/animal/all');
+    });
+});
+
+
+// Delete a address for the given address_id
 router.get('/delete', function(req, res){
     if(req.query.animals_id == null) {
         res.send('animals_id is null');
@@ -95,54 +94,11 @@ router.get('/delete', function(req, res){
             }
             else {
                 //poor practice, but we will handle it differently once we start using Ajax
-                res.redirect(302, '/animals/all');
+                res.redirect(302, '/animal/all');
             }
         });
     }
 });
 
-/* LAB 12 */
-// router.get('/edit', function(req, res){
-//     if(req.query.animals_id == null) {
-//         res.send('A animals id is required');
-//     }
-//     else {
-//         animals_dal.edit(req.query.animals_id, function(err, result){
-//             res.render('animals/animalsUpdate', {school: result[0][0], address: result[1]});
-//         });
-//     }
-// });
-/*
-router.get('/edit2', function(req, res){
-    if(req.query.animals_id == null) {
-        res.send('A animals id is required');
-    }
-    else {
-        animals_dal.getById(req.query.animals_id, function(err, animals){
-            account_dal.getAll(function(err, account) {
-                company_dal.getAll(function(err, company) {
-                    company_dal.getByIdX(req.query.animals_id, function(err, companyX) {
-                                skill_dal.getAll(function(err, skill) {
-                                    skill_dal.getByIdX(req.query.animals_id, function(err, skillX) {
-                                        console.log("company: "+ company.length);
-                                        console.log("companyX: "+ companyX);
-                                        res.render('animals/animalsUpdate', {animals: animals[0], account: account, company: company, school: school, skill: skill,
-                                            companyX: companyX, schoolX: schoolX, skillX: skillX });
-                                    });
-                                });
-                        });
-                    });
-                });
-            });
-        };
-});
-*/
-router.get('/update', function(req, res){
-    animals_dal.delete(req.query.animals_id, function(err, result){
-        animals_dal.insert(req.query, function (err, result) {
-            res.redirect(302, '/animal/all');
-        });
-    });
-});
 
 module.exports = router;
